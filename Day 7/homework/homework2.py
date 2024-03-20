f295d98de9de33637d5bb4189fd6a1518ab39a3b
zedazneli-niko <lipartelianiakaki32@gmail.com>
2024-03-20 21:46:38 +0400

    commit

---
+++ Day 7/homework/homework2.py
@@ -0,0 +1,19 @@
+num1=int(input("enter num1: "))
+num2=int(input("enter num2: "))
+num3=int(input("enter num3: "))
+num4=int(input("enter num4: "))
+num5=int(input("enter num5: "))
+
+print(num1 + num2 + num3 + num4 + num5)
+print(num1 - num2 - num3 - num4 - num5)
+print(num1 * num5 * num3 * num4 * num2)
+print(num1 / num2 / num3 / num4 / num5)
+
+
+
+
+
+
+
+
+
