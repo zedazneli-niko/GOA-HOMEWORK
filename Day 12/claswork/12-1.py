e62bbe412eaf78950ea903b77c6ce7c8e0d437ee
zedazneli-niko <lipartelianiakaki32@gmail.com>
2024-04-28 18:49:47 +0400

    commit

---
+++ Day 12/claswork/12-1.py
@@ -0,0 +1,23 @@
+#number 1
+#for nikolozi in range(1,6):
+#    print(nikolozi) 
+# number 2
+for i in range(6,):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day 12/homework/davaleba 1.py
@@ -0,0 +1,5 @@
+for i in range(11):
+    print(i) 
+
+for I in range(-1,11,0):
+    print(I)
